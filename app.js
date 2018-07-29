5559414037859ff4b8aac70a5a652edc15fe46b0
Dylan Beckwith <dylan@wintermute.industries>
2018-07-29 16:11:01 -0700

    init (hello world)

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.get('/', (req, res) => res.send('Hello world'));
+
+app.listen(3000);
+console.log('Server is running on port 3000');
