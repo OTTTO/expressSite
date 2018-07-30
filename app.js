7a7f3db8149d8a058480cd1107c20579919941b2
Dylan Beckwith <dylan@wintermute.industries>
2018-07-30 16:24:20 -0700

    views (and css)

--- app.js
+++ app.js
@@ -5,10 +5,20 @@ var nodemailer = require('nodemailer');
 
 var app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.send('Hello world'));
+app.get('/', (req, res) => {
+	res.render('index', {title: 'WELCOME'});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about');
+});
 
 app.listen(3000);
 console.log('Server is running on port 3000');
